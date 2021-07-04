b49db5b06fb3d9ff7ea33f2303def0a93b013caf
PhillMed <parapushon@gmail.com>
2021-07-04 10:39:59 +0300

    Исправил формулу в 1 задании, а также заменил int на long, чтобы можно было брать большие значения. Также сделал 2 и 4е задание. в 3м задании не смог придумать алгоритм, то ли создавать 4 массива, то ли написать кучу строк условий и циклов. В общем, может, до вечера понедельнка придумаю.

--- Lesson4/src/Homework4.java
+++ Lesson4/src/Homework4.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 public class Homework4 {
     public static void main(String[] args) {
         amebaPer3Hours(); //1 задание, про амёб, где каждые 3 часа их количество удваивается.
+        multiInSuMM(); //2 задание, где надо найти произвдение, не используя умножение.
+//        massives4massives(); //3 задание, где надо нарисовать треугольники в массивах
+        info4count(); // 4 задание, где надо узнать информацию о числе
     }
 
     public static void amebaPer3Hours() {
@@ -10,8 +13,8 @@ public class Homework4 {
         int amebs = in.nextInt();
         System.out.println("Каждые 3 часа Амёба делится на двое.\nСколько времени вы хотите пропустить?");
         int time = in.nextInt();
-        int count = time / 3 +1;
-        int amebsFinal = count * amebs;
+        int count = time / 3;
+        long amebsFinal = (long) (Math.pow(2, count) * amebs);
         String x = null;
         if (time >= 0) {
             int c100 = time % 100;
@@ -28,8 +31,8 @@ public class Homework4 {
         }
         String y = null;
         if (amebsFinal >= 0) {
-            int d100 = amebsFinal % 100;
-            int c10 = d100 % 10;
+            long d100 = amebsFinal % 100;
+            long c10 = d100 % 10;
             if (d100 >= 11 && d100 <= 19) {
                 y = "амёб";
             } else if (c10 >= 2 && c10 <= 4) {
@@ -41,5 +44,54 @@ public class Homework4 {
             }
         }
         System.out.println("Спустя " + time + " час" + x + " будет " + amebsFinal + " " + y);
+    }
+
+    public static void multiInSuMM() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Попробуем перемножить 2 числа без умножения\nВведите число a:");
+        int a = in.nextInt();
+        System.out.println("Введите число b:");
+        int b = in.nextInt();
+        int summ = a;
+        if (b > 0) {
+            for (int i = 1; i < b; i++) {
+                summ += a;
+            }
+        } else if (b < 0) {
+            for (int i = -1; i > b; i--) {
+                summ += a;
+            }
+        } else {
+            summ = 0;
+        }
+        System.out.println("Произведение равно: " + summ);
+    }
+
+/*
+    public static void massives4massives (){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Сейчас будут нарисованы матричные треугольники.\nВведите размер матрицы: ");
+        int size = in.nextInt();
+        int[][] matrix = new int[size][size];
+    }
+}*/
+
+    public  static  void info4count () {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Сейчас будет информация о введённом числе.\nВведите число: ");
+        int count = in.nextInt();
+        String zero;
+        if (count > 0) {
+            zero = " - это положительное число, его длинна: ";
+        }
+        else if (count < 0) {
+            zero = " - это отрицательное число, его длинна: ";
+        }
+        else {
+            zero = " - это бублик :), его длинна: ";
+        }
+        int lengthNumber = (String.valueOf(count)).length();
+        System.out.println(count + zero + lengthNumber);
+
     }
 }
