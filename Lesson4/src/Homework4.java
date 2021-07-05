5cac3cf1d174a90dc44b6bd0455d435e4ef12ffd
PhillMed <parapushon@gmail.com>
2021-07-05 12:00:59 +0300

    В первом задании я хочу оставить лонг, т.к. на проверку часто ставлю 123. здесь же на 123 выйдет число сильно превышающее int. 2 Задание. Поправил, теперь, вроде, всё ок. 3 Задание не сделано. 4 Задание. Всё также считает символ "-" как цифру. 5 Задание. Вроде ок. 6 Задание. Не получилось присвоить нужный индекс. 7 Задание. В попытке вывести второй массив, заменив каждое второе значение на 0, заменят на 0 всё.

--- Lesson4/src/Homework4.java
+++ Lesson4/src/Homework4.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 public class Homework4 {
     public static void main(String[] args) {
@@ -5,6 +6,9 @@ public class Homework4 {
         multiInSuMM(); //2 задание, где надо найти произвдение, не используя умножение.
 //        massives4massives(); //3 задание, где надо нарисовать треугольники в массивах
         info4count(); // 4 задание, где надо узнать информацию о числе
+        massive13531(); // 5 задание, надо вывести массив нечётных чисел, а после выдать его в обратном порядке
+        massiveMaxCount(); // 6 задание. Вывести порядковый номер максимального числа в массиве
+        massiveToZero();
     }
 
     public static void amebaPer3Hours() {
@@ -46,20 +50,20 @@ public class Homework4 {
         System.out.println("Спустя " + time + " час" + x + " будет " + amebsFinal + " " + y);
     }
 
-    public static void multiInSuMM() {
+    public static void multiInSuMM() { //2 задание, где надо найти произвдение, не используя умножение.
         Scanner in = new Scanner(System.in);
         System.out.println("Попробуем перемножить 2 числа без умножения\nВведите число a:");
         int a = in.nextInt();
         System.out.println("Введите число b:");
         int b = in.nextInt();
-        int summ = a;
+        int summ = 0;
         if (b > 0) {
-            for (int i = 1; i < b; i++) {
-                summ += a;
-            }
-        } else if (b < 0) {
-            for (int i = -1; i > b; i--) {
-                summ += a;
+            for (int i = 0; i < b; i++) {           //
+                summ += a;                          //
+            }                                       //<-----------------Подумать над формулой
+        } else if (b < 0) {                         //
+            for (int i = 0; i > b; i--) {          //
+                summ -= a;
             }
         } else {
             summ = 0;
@@ -68,7 +72,7 @@ public class Homework4 {
     }
 
 /*
-    public static void massives4massives (){
+    public static void massives4massives (){ //3 задание, где надо нарисовать треугольники в массивах
         Scanner in = new Scanner(System.in);
         System.out.println("Сейчас будут нарисованы матричные треугольники.\nВведите размер матрицы: ");
         int size = in.nextInt();
@@ -76,22 +80,86 @@ public class Homework4 {
     }
 }*/
 
-    public  static  void info4count () {
+    public static void info4count() { // 4 задание, где надо узнать информацию о числе
         Scanner in = new Scanner(System.in);
         System.out.println("Сейчас будет информация о введённом числе.\nВведите число: ");
         int count = in.nextInt();
         String zero;
         if (count > 0) {
             zero = " - это положительное число, его длинна: ";
-        }
-        else if (count < 0) {
+        } else if (count < 0) {
             zero = " - это отрицательное число, его длинна: ";
-        }
-        else {
+        } else {
             zero = " - это бублик :), его длинна: ";
         }
-        int lengthNumber = (String.valueOf(count)).length();
+        int lengthNumber = String.valueOf(count).length();
         System.out.println(count + zero + lengthNumber);
+    }
 
+    public static void massive13531() { // 5 задание. Вывести массив нечётных чисел, а после выдать его в обратном порядке
+        int x = 0;
+        int y = 0;
+        for (int i = 1; i <= 99; i++) {
+            if (i % 2 != 0) {
+                x++;
+            }
+        }
+        int[] massive = new int[x];
+        for (int i = 1; i <= 99; i++) {
+            if (i % 2 != 0) {
+                massive[y] = i;
+                System.out.print(massive[y] + " ");
+                y++;
+            }
+        }
+        System.out.println(" ");
+        for (int i = massive.length - 1; i >= 0; i--) {
+            System.out.print(massive[i] + " ");
+        }
+    }
+
+    public static void massiveMaxCount() { // 6 задание. Вывести порядковый номер максимального числа в массиве
+        Scanner in = new Scanner(System.in);
+        System.out.println("Задание 6. Номер max числа в массиве\nВведите размер массива: ");
+        int size = in.nextInt();
+        int number = 0;
+        int[] massive = new int[size];
+        int max = massive[0];
+        System.out.println("Введите максимальное значение рандома: ");
+        int maxR = in.nextInt();
+        Random random = new Random();
+        for (int i = 0; i < size; i++) {
+            int value = random.nextInt(maxR);
+            massive[i] = value;
+            System.out.print(value + " ");
+            for (int x : massive) {
+                if (x >= max) {
+                    max = x;
+                    number = i;
+                }
+            }
+        }
+        System.out.println();
+        System.out.println("Максимальный элемент " + max + ", индекс его последнего вхождения в массив = " + number);
+    }
+
+    public static void massiveToZero() { //7 Задание. массив из 20 рандомных чисел. Каждое чётное заменить на 0
+        System.out.println("Задание 7. Замена чётных элементов массива на нули");
+        int[] massive = new int[20];
+        Random random = new Random();
+        for (int i = 0; i < 20; i++) {
+            int value = random.nextInt(20);
+            massive[i] = value;
+            System.out.print(value + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < 20; i++) {
+            int value = random.nextInt(20);
+            if (i % 2 != 0) {
+                value = 0;
+                massive[i] = value;
+                System.out.print(value + " ");
+            }
+        }
     }
 }
